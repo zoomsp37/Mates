e7af6db1fd4ab57aff9a9672197065eae73916b2
zoomsp37 <alberto@lesmes.info>
2025-03-11 16:44:51 +0100

    hola

--- SCD 3x3.py
+++ SCD 3x3.py
@@ -18,5 +18,23 @@ for i in range(3):
     for j in range(3):
         t_i[i] += A[i][j]*[x,y,z][j]
 
-print(t_i)
+print(f'La matriz de coeficientes es: {A}')
+print(f'El vector solución es: [{x},{y},{z}]')
+print(f'El vector resultado es: {t_i}')
 
+r_1=''
+
+for i in range(3):
+    for j in range(3):
+        if A[i][j] == 1:
+            r_1 += f'x_{j+1}'
+        elif A[i][j] == -1:
+            r_1 += f'-x_{j+1}'
+        elif A[i][j] > 0:
+            r_1 += f'{A[i][j]}x_{j+1}'
+        elif A[i][j] < 0:
+            r_1 += f'{A[i][j]}x_{j+1}'
+        
+            
+
+print(f'r_1')
