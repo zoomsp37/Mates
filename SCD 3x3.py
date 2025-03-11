213787dc5c4f345546256adce80d3f3131352b77
zoomsp37 <alberto@lesmes.info>
2025-03-11 21:30:22 +0100

    nice

--- SCD 3x3.py
+++ SCD 3x3.py
@@ -6,6 +6,9 @@ x = random.randint(-r,r)
 y = random.randint(-r,r)
 z = random.randint(-r,r)
 
+sol_vec = [x,y,z]
+inc_vec =['x','y','z']
+
 A = [[random.randint(-r,r), random.randint(-r,r), random.randint(-r,r)], 
      [random.randint(-r,r), random.randint(-r,r), random.randint(-r,r)], 
      [random.randint(-r,r), random.randint(-r,r), random.randint(-r,r)]]
@@ -20,19 +23,22 @@ print(f'La matriz de coeficientes es: {A}')
 print(f'El vector solución es: [{x},{y},{z}]')
 print(f'El vector de términos independientes es: {t_i}')
 
-# r_1=''
+r_1=''
 
-# for i in range(3):
-#     for j in range(3):
-#         if A[i][j] == 1:
-#             r_1 += f'x_{j+1}'
-#         elif A[i][j] == -1:
-#             r_1 += f'-x_{j+1}'
-#         elif A[i][j] > 0:
-#             r_1 += f'{A[i][j]}x_{j+1}'
-#         elif A[i][j] < 0:
-#             r_1 += f'{A[i][j]}x_{j+1}'
-        
-            
+for i in range(3):
+    for j in range(3):
+        if A[i][j] == 1 and j == 0:
+            r_1 += inc_vec[j]
+        elif A[i][j] == 1 and j != 0:
+            r_1 = r_1 + '+' + inc_vec[j]
+        elif A[i][j] == -1:
+            r_1 = r_1 + '-' + inc_vec[j]
+        elif A[i][j] == 0:
+            continue
+        elif A[i][j] > 0:
+            r_1 = r_1 + '+ ' f'{A[i][j]}' + inc_vec[j]
+        elif A[i][j] < 0:
+            r_1 = r_1 + f'{A[i][j]}' + inc_vec[j]
+    r_1 = r_1 + ' = ' + f'{t_i[i]}' +', '
 
-# print(f'r_1')
+print(f'La respuesta es:' + r_1)
